3b03b4ca83719efb7f7fc769345641e055b36ad2
PHClaus <phhpro@gmail.com>
2016-12-17 10:41:54 +0100

    Update registered.php

--- inc/registered.php
+++ inc/registered.php
@@ -1,13 +1,13 @@
 <?php
+//** load config
 include ('./config.php');
 
 //** deny direct access
-if (($_SERVER['HTTP_REFERER'] == '') || ($_SERVER['HTTP_HOST'] != $ac_dom)) {
-  header("Location: ../");
+if (($_SERVER['HTTP_REFERER'] == '') || 
+    ($_SERVER['HTTP_HOST'] != $ac_dom)) {
+  header('Location: ../');
   exit;
 }
+
+//** registered users are auto-added below
 ?>
-
-ZGVtbw==|ZGVtbw==
-
-dGVzdA==|dGVzdA==
