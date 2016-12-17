38cac4c1c3a6f726cbf3414d10bf41627f680551
PHClaus <phhpro@gmail.com>
2016-12-17 10:42:22 +0100

    Update init_form.php

--- inc/init_form.php
+++ inc/init_form.php
@@ -1,6 +1,8 @@
 <?php
+//** init session
 session_start();
 
+//** load config
 include ('./config.php');
 
 //** link and encode user name and password
@@ -11,29 +13,28 @@ $ac_pass = base64_encode($_POST['ac_pass']);
 if (isset ($_POST['ac_register'])) {
 
   //** check empty values
-  if (($ac_user != '') && ($ac_pass != '')) {
+  if (($ac_user != '') && 
+      ($ac_pass != '')) {
 
-    //** check if user name is banned or already registered
-    if ((strpos(file_get_contents($ac_ban), base64_decode($ac_user)) !== FALSE) || (strpos(file_get_contents($ac_reg), base64_decode($ac_user)) !== FALSE)) {
-
-      //** return to login
+    //** banned user name or already registered
+    if ((strpos(file_get_contents($ac_ban), base64_decode($ac_user)) !== FALSE) || 
+        (strpos(file_get_contents($ac_reg), base64_decode($ac_user)) !== FALSE)) {
       header("Location: $ac_dir");
       exit;
     } else {
       //** add new user to data file
-      $ac_reg_new = "\n" . $ac_user . '|' . $ac_pass;
+      $ac_reg_new = $ac_user . '|' . $ac_pass . "\n";
       file_put_contents($ac_reg, $ac_reg_new, FILE_APPEND);
 
       //** init session
-      $_SESSION['ac_chat'] = $ac_user;
+      $_SESSION['ac_user'] = $ac_user;
+      $_SESSION['ac_time'] = time();
 
-      //** update users online counter
+      //** update users online counter and load chat
       $ac_on_cur = file_get_contents($ac_cur);
       $ac_on_val = $ac_on_cur;
-      $ac_on_cur = ($ac_on_val +1);
+      $ac_on_cur = ($ac_on_val + 1);
       file_put_contents($ac_cur, $ac_on_cur);
-
-      //** load chat
       header("Location: $ac_chm");
       exit;
     }
@@ -54,22 +55,22 @@ if (isset ($_POST['ac_login'])) {
   }
 
   //** check if user name and password match
-  if ((strpos(file_get_contents($ac_reg), $ac_user) !== FALSE) && (strpos(file_get_contents($ac_reg), $ac_pass) !== FALSE)) {
+  if ((strpos(file_get_contents($ac_reg), $ac_user) !== FALSE) && 
+      (strpos(file_get_contents($ac_reg), $ac_pass) !== FALSE)) {
 
     //** init session
-    $_SESSION['ac_chat'] = $ac_user;
+    $_SESSION['ac_time'] = time();
+    $_SESSION['ac_user'] = $ac_user;
 
-    //** update users online counter
+    //** update users online counter and load chat
     $ac_on_cur = file_get_contents($ac_cur);
     $ac_on_val = $ac_on_cur;
-    $ac_on_cur = ($ac_on_val +1);
+    $ac_on_cur = ($ac_on_val + 1);
     file_put_contents($ac_cur, $ac_on_cur);
-
-    //** load chat
     header("Location: $ac_chm");
     exit;
   } else {
-    //** no match, return to login
+    //** no match -- back to login
     header("Location: $ac_dir");
     exit;
   }
