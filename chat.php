0054c1209dd68682233bd7929a9758a4370fbed5
PHClaus <phhpro@gmail.com>
2016-12-17 10:41:22 +0100

    Update chat.php

--- chat.php
+++ chat.php
@@ -1,10 +1,13 @@
 <?php
+//** init session
 session_start();
 
+//** load config
 include ('./inc/config.php');
 
 //** check session
-if (!isset ($_SESSION['ac_chat'])) {
+if ((!isset ($_SESSION['ac_time'])) || 
+    (!isset ($_SESSION['ac_user']))) {
   header("Location: $ac_dir");
   exit;
 }
@@ -22,29 +25,26 @@ if (!isset ($_SESSION['ac_chat'])) {
     <link rel="stylesheet" href="<?php echo $ac_www . $ac_inc; ?>style.css" type="text/css">
   </head>
   <body>
-<?php
-include ($ac_hdr);
-?>
+<?php include ($ac_hdr); ?>
     <object data="<?php echo $ac_www . $ac_chp; ?>" type="text/html">Failed to render data object!</object>
     <div id="ac_menu">
       <form action="<?php echo $ac_www . $ac_chf; ?>" method="POST" id="ac_chat">
         <div id="ac_char">Text <span id="ac_count"></span></div>
         <div>
-          <textarea rows="3" cols="40" name="ac_text" maxlength="256" title="Please enter your message" onkeyup="ac_count('ac_text');"></textarea>
+          <textarea rows="4" cols="40" name="ac_text" maxlength="256" title="Type here to enter your message" onkeyup="ac_count('ac_text');"></textarea>
         </div>
         <div>
-          <input type="hidden" name="ac_user" value="<?php echo $_SESSION['ac_chat']; ?>">
-          <input type="submit" name="ac_save" value="Save" title="Click here to save your session">
-          <input type="submit" name="ac_quit" value="Quit" title="Click here to quit your session">
-          <input type="submit" name="ac_push" value="Push" title="Click here to update the chat log">
+          <input type="hidden" name="ac_user" value="<?php echo $_SESSION['ac_user']; ?>">
+          <input type="submit" name="ac_quit" value="Quit" title="Click here to quit the current session">
+          <input type="submit" name="ac_save" value="Save" title="Click here to download and save the current session">
+          <input type="submit" name="ac_push" value="Push" title="Click here to manually update the current session">
           <input type="submit" name="ac_post" value="Post" title="Click here to post your message">
         </div>
       </form>
-<?php
-include ($ac_ftr);
-?>
+<?php include ($ac_ftr); ?>
     </div>
     <script type="text/javascript">
+    //** character counter
     var ac_cmax = 256;
     var ac_cdiv = "ac_count";
 
