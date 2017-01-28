1333b3fe19cd75e218c1d495db864080857f02e1
PHClaus <phhpro@gmail.com>
2017-01-28 17:52:03 +0100

    Update init_form.php

--- inc/init_form.php
+++ inc/init_form.php
@@ -5,20 +5,23 @@ session_start();
 //** load config
 include ('./config.php');
 
-//** link and encode user name and password
-$ac_user = base64_encode($_POST['ac_user']);
-$ac_pass = base64_encode($_POST['ac_pass']);
+//** link name and password
+$ac_user = $_POST['ac_user'];
+$ac_pass = $_POST['ac_pass'];
 
 //** register
 if (isset ($_POST['ac_register'])) {
 
-  //** check empty values
-  if (($ac_user != '') && 
-      ($ac_pass != '')) {
+  //** check empty name and password
+  if (($ac_user == '') || 
+      ($ac_pass == '')) {
+    header("Location: $ac_dir");
+    exit;
+  } else {
 
-    //** banned user name or already registered
-    if ((strpos(file_get_contents($ac_ban), base64_decode($ac_user)) !== FALSE) || 
-        (strpos(file_get_contents($ac_reg), base64_decode($ac_user)) !== FALSE)) {
+    //** check registered or banned name
+    if ((strpos(file_get_contents($ac_reg), $ac_user) !== false) || 
+        (strpos(file_get_contents($ac_ban), $ac_user) !== false)) {
       header("Location: $ac_dir");
       exit;
     } else {
@@ -38,40 +41,44 @@ if (isset ($_POST['ac_register'])) {
       header("Location: $ac_chm");
       exit;
     }
-  } else {
-    //** return to login
-    header("Location: $ac_dir");
-    exit;    
   }
 }
 
 //** login
 if (isset ($_POST['ac_login'])) {
 
-  //** check banned user name
-  if ((strpos(file_get_contents($ac_ban), $ac_user) !== FALSE)) {
+  //** check empty name and password
+  if (($ac_user == '') || 
+      ($ac_pass == '')) {
     header("Location: $ac_dir");
-    exit;
-  }
-
-  //** check if user name and password match
-  if ((strpos(file_get_contents($ac_reg), $ac_user) !== FALSE) && 
-      (strpos(file_get_contents($ac_reg), $ac_pass) !== FALSE)) {
-
-    //** init session
-    $_SESSION['ac_time'] = time();
-    $_SESSION['ac_user'] = $ac_user;
-
-    //** update users online counter and load chat
-    $ac_on_cur = file_get_contents($ac_cur);
-    $ac_on_val = $ac_on_cur;
-    $ac_on_cur = ($ac_on_val + 1);
-    file_put_contents($ac_cur, $ac_on_cur);
-    header("Location: $ac_chm");
     exit;
   } else {
-    //** no match -- back to login
-    header("Location: $ac_dir");
-    exit;
+
+    //** check banned name
+    if ((strpos(file_get_contents($ac_ban), $ac_user) !== FALSE)) {
+      header("Location: $ac_dir");
+      exit;
+    }
+
+    //** check if name and password match
+    if ((strpos(file_get_contents($ac_reg), $ac_user) !== FALSE) && 
+        (strpos(file_get_contents($ac_reg), $ac_pass) !== FALSE)) {
+
+      //** init session
+      $_SESSION['ac_time'] = time();
+      $_SESSION['ac_user'] = $ac_user;
+
+      //** update users online counter and load chat
+      $ac_on_cur = file_get_contents($ac_cur);
+      $ac_on_val = $ac_on_cur;
+      $ac_on_cur = ($ac_on_val + 1);
+      file_put_contents($ac_cur, $ac_on_cur);
+      header("Location: $ac_chm");
+      exit;
+    } else {
+      //** no match -- back to login
+      header("Location: $ac_dir");
+      exit;
+    }
   }
 }
