09cd527976bf4a7d9cc65335621b43bcd52e7563
PHClaus <phhpro@gmail.com>
2017-03-19 14:49:55 +0100

    Update banned.php

--- inc/banned.php
+++ inc/banned.php
@@ -1,34 +1,22 @@
 <?php
-//** deny direct access
-$ac_dat = 'n';
-
-if ($ac_dat == 'n') {
-  header('Location: ../');
-  exit;
-}
-//** basic list of banned names -- some regex voodoo may be better
-?>
-admin
-ass
-asshole
-bitch
-bullshit
-cunt
-dickhead
-fuck
-fuckall
-fucker
-fuckoff
-fuckthis
-fuckshit
-fuckyou
-moderator
-piss
-pisser
-pussy
-root
-shit
-staff
-suck
-sucker
-webmaster
+/*
+ * banned names
+ *
+ * regex matches anything beginning with $string
+ *
+ * $string will match string, stringfoo
+ * but not mystring, mystringfoo
+ */
+$ac_nono = array (
+                  preg_match('/\Badmin(.+?)\B/', $ac_user),
+                  preg_match('/\Bass(.+?)\B/', $ac_user),
+                  preg_match('/\Bcunt(.+?)\B/', $ac_user),
+                  preg_match('/\Bdick(.+?)\B/', $ac_user),
+                  preg_match('/\Bfuck(.+?)\B/', $ac_user),
+                  preg_match('/\Bmoderat(.+?)\B/', $ac_user),
+                  preg_match('/\Bpiss(.+?)\B/', $ac_user),
+                  preg_match('/\Broot(.+?)\B/', $ac_user),
+                  preg_match('/\Bsuck(.+?)\B/', $ac_user),
+                  preg_match('/\Btest(.+?)\B/', $ac_user),
+                  preg_match('/\Bwebmas(.+?)\B/', $ac_user),
+                 );
