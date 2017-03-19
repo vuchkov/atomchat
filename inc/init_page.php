3dadd67c380dd026691a911a1fd784d2cd304381
PHClaus <phhpro@gmail.com>
2017-03-19 11:40:26 +0100

    Update init_page.php

--- inc/init_page.php
+++ inc/init_page.php
@@ -1,9 +1,8 @@
-<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="style.css" />
 <div id="ac_page">
   <p>Welcome to <strong>Atom Chat</strong>.</p>
-  <p>Please login to start chatting or register a new account. It's FREE!</p>
-  <p>Just enter your desired user name and password and then click the <em>Register</em> button.</p>
-  <p>Note that both <em>Login</em> and <em>Register</em> default to do nothing if:</p>
+  <p>Please login to start chatting or register a new account.</p>
+  <p><em>Login</em> and <em>Register</em> default to do nothing if:</p>
   <ul>
     <li>Required values are missing or invalid.</li>
     <li>The user name is already registered or banned.</li>
