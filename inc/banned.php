7f04eef3b467ca273bc42dd8806e0a82b7b4eedd
PHClaus <phhpro@gmail.com>
2017-03-22 21:01:16 +0100

    Update banned.php

--- inc/banned.php
+++ inc/banned.php
@@ -1,22 +1,15 @@
 <?php
-/*
- * banned names
- *
- * regex matches anything beginning with $string
- *
- * $string will match string, stringfoo
- * but not mystring, mystringfoo
- */
-$ac_nono = array (
-                  preg_match('/\Badmin(.+?)\B/', $ac_user),
-                  preg_match('/\Bass(.+?)\B/', $ac_user),
-                  preg_match('/\Bcunt(.+?)\B/', $ac_user),
-                  preg_match('/\Bdick(.+?)\B/', $ac_user),
-                  preg_match('/\Bfuck(.+?)\B/', $ac_user),
-                  preg_match('/\Bmoderat(.+?)\B/', $ac_user),
-                  preg_match('/\Bpiss(.+?)\B/', $ac_user),
-                  preg_match('/\Broot(.+?)\B/', $ac_user),
-                  preg_match('/\Bsuck(.+?)\B/', $ac_user),
-                  preg_match('/\Btest(.+?)\B/', $ac_user),
-                  preg_match('/\Bwebmas(.+?)\B/', $ac_user),
-                 );
+//** deny direct access
+$ac_dat = 'n';
+
+if ($ac_dat == 'n') {
+  header('Location: ../');
+  exit;
+}
+//** add banned names below
+?>
+admin
+administrator
+moderator
+root
+webmaster
