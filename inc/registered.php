82836d89a90198a38561d4d5cc3f5fa13a05cc5f
PHClaus <phhpro@gmail.com>
2017-04-29 16:52:30 +0200

    Update registered.php

--- inc/registered.php
+++ inc/registered.php
@@ -1,9 +1,9 @@
 <?php
 //** deny direct access
-$ac_dat = 'n';
+$ac_dat = "n";
 
-if ($ac_dat == 'n') {
-  header('Location: ../');
+if ($ac_dat == "n") {
+  header("Location: ../");
   exit;
 }
 //** registered users are auto-added below
