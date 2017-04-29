3805addef4ea3446d692681dc89b8640b1137328
PHClaus <phhpro@gmail.com>
2017-04-29 13:49:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ if (isset ($_SESSION['ac_time'])) {
 <html lang="en-GB">
   <head>
     <title>Atom Chat Login</title>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta charset="UTF-8" />
     <meta name="language" content="en-GB" />
     <meta name="description" content="Atom Chat free PHP chat script" />
     <meta name="keywords" content="Atom Chat" />
@@ -54,14 +54,13 @@ if (isset ($_SESSION['ac_time'])) {
       <form action="<?php echo $ac_www . $ac_nif; ?>" method="POST" id="ac_login">
         <div>
           <label for="ac_user">User</label>
-          <input type="text" name="ac_user" id="ac_user" maxlength="10" title="Please enter your user name" />
+          <input type="text" name="ac_user" id="ac_user" maxlength="16" title="Please enter your user name" />
         </div>
         <div>
           <label for="ac_pass">Pass</label>
-          <input type="password" name="ac_pass" id="ac_pass" maxlength="10" title="Please enter your password" />
+          <input type="password" name="ac_pass" id="ac_pass" maxlength="16" title="Please enter your password" />
         </div>
         <div>
-          <input type="submit" name="ac_register" value="Register" title="Click here to register" />
           <input type="submit" name="ac_login" value="Login" title="Click here to login" />
         </div>
       </form>
