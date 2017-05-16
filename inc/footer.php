1fcf8ed2fc5cb17b17cc846c0d1c42477521ada0
PHClaus <phhpro@gmail.com>
2017-05-16 11:20:19 +0200

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,1 +1,1 @@
-      <div id=ac_footer><a href="http://phclaus.com/php-scripts/atomchat/" title="Powered by Atom Chat. Click here to get a free copy.">Powered by Atom Chat v<?php echo $ac_ver; ?></a></div>
+      <div id=ac_footer><a href="http://phclaus.com/php-scripts/#AtomChat" title="Powered by Atom Chat. Click here to get a free copy.">Powered by Atom Chat v<?php echo $ac_ver; ?></a></div>
